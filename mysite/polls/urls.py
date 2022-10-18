b6fc510c5860e4132ecc452869331f90e59c125a
jhonataguerra <jhonataguerra@outlook.com.br>
2022-10-18 15:27:52 -0300

    Initial templates and adjusts in views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,16 @@ from . import views
 
 # Home page
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
 ]
 
+# Details, results and votes
+urlpatterns += [
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
